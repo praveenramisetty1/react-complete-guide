51cd1d8fa74b25c52797fee4c1b68817c6e3fbe9
Praveen Ramisetty <pramisetty@adaptivebiotech.com>
2022-09-17 21:50:23 -0400

    Rendering Content Conditionally

--- src/App.js
+++ src/App.js
@@ -35,6 +35,8 @@ const App = props => {
     })
   };
   
+  const [showPersons, setShowPersons] = useState(false);
+
   const style = {
     backgroundColor: 'white',
     font: 'inherit',
@@ -43,24 +45,33 @@ const App = props => {
     cursor: 'pointer'
   }
 
+  const togglePersonHandle = () => {
+    setShowPersons(!showPersons);
+  }
+
   return (
     <div className="App">
       <h1> Hi I'm a React App</h1>
       <p>This is really working!</p>
-      <button style={style} onClick={() => switchNameHandler('Maxmilan!!')}> Switch Name </button>
-      <Person 
-        name={personsState.persons[0].name} 
-        age={personsState.persons[0].age}/>
-      <Person 
-        name={personsState.persons[1].name} 
-        age={personsState.persons[1].age}
-        click={switchNameHandler.bind(this, 'Max!!')}
-        changed={nameChangeHandle}> 
-            My Hobbies: Racing
-        </Person>
-      <Person 
-        name={personsState.persons[2].name} 
-        age={personsState.persons[2].age}/>
+      <button style={style} onClick={togglePersonHandle}> Toggle Persons </button>
+      {
+        (showPersons === true) ?
+          <div>
+            <Person 
+              name={personsState.persons[0].name} 
+              age={personsState.persons[0].age}/>
+            <Person 
+              name={personsState.persons[1].name} 
+              age={personsState.persons[1].age}
+              click={switchNameHandler.bind(this, 'Max!!')}
+              changed={nameChangeHandle}> 
+                  My Hobbies: Racing
+              </Person>
+            <Person 
+              name={personsState.persons[2].name} 
+              age={personsState.persons[2].age}/>
+          </div> : null
+      }
     </div>
 
     // React.createElement('div',null, 
